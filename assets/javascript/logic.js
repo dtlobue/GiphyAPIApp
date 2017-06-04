cb95fd95f58c0daf6f5cec144527f0473164882c
Daniel LoBue <dlobue@Daniels-MBP.lan>
2017-06-04 13:07:47 -0700

    some more progress

---
+++ assets/javascript/logic.js
@@ -0,0 +1,16 @@
+// need initial document on body line of code to ensure page loads first before code runs
+
+//declaration of global variables, incl array of initial buttons
+
+//for loop running through variable holding the array and dynamically populating buttons on DOM
+
+//Clicking on a button makes it go to the Giphy API and pull 10 *static* GIFs:
+// So, an on.click event. Make sure to include $(this) in it, which makes sure only the button clicked sends
+// it's VALUE to Giphy API
+
+//When clicking on one of the static GIFs, it animates. When clicked again, it reverts to static.
+// So: on click event(s)? See: giphy-example-hw-6.html
+
+//Search box takes submitted data, adds it to array, and goes through same process as other buttons
+
+//function initializations?
